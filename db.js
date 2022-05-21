d84cc350c0bed79ec1f5829094dcf1c4f5343d67
oghomwen <osazuwaoghomwen@gmail.com>
2022-05-21 11:53:28 +0200

    changes

--- db.js
+++ db.js
@@ -10,9 +10,17 @@
 // module.exports = db;
 
 
+// const Sequelize = require('sequelize');
+// const db = new Sequelize ('pollution', 'root', '', {
+//     host:'127.0.0.1',
+//     dialect:'mysql'
+// });
+
+// module.exports = db;
+
 const Sequelize = require('sequelize');
-const db = new Sequelize ('pollution', 'root', '', {
-    host:'127.0.0.1',
+const db = new Sequelize ('heroku_7d4cc3976438d6a', 'bf83118541a823', '5b220469', {
+    host:'eu-cdbr-west-02.cleardb.net',
     dialect:'mysql'
 });
 
